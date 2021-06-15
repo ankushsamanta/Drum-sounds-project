9003c9ad112751f29132363bc162cb6c672d3749
Ankush Samanta <58259657+ankushsamanta@users.noreply.github.com>
2021-06-15 21:13:16 +0530

    Add files via upload

---
+++ Drum Project/index.js
@@ -0,0 +1,64 @@
+
+document.querySelector(".m").addEventListener("click", click1);
+
+function click1() {
+   
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    
+}
+
+document.querySelector(".a").addEventListener("click", click2);
+
+function click2() {
+   
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+}
+
+
+document.querySelector(".c").addEventListener("click", click3);
+
+function click3() {
+   
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+}
+
+
+document.querySelector(".k").addEventListener("click", click4);
+
+function click4() {
+   
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+}
+
+
+document.querySelector(".p").addEventListener("click", click5);
+
+function click5() {
+   
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+}
+
+
+document.querySelector(".i").addEventListener("click", click6);
+
+function click6() {
+   
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+}
+
+
+document.querySelector(".h").addEventListener("click", click7);
+
+function click7() {
+   
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+}
+
+
